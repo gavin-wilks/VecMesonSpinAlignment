b9f8096e44f2c55f94bdfec0b90741debf3a2236
Xu Sun <sunxuhit@gmail.com>
2018-02-16 12:58:17 -0800

    v0.27: update submit macro for ZDCSMD histogram

--- FillSpinAlignment/submit/submit.sh
+++ FillSpinAlignment/submit/submit.sh
@@ -5,8 +5,11 @@ date
 
 if [ $# -eq 0 ]
 then
-  # sbatch --account rhstar --ntasks=1 --array=0-9 --time=01:00:00 FillSpinAlignment.slr # test mode
-  sbatch --account rhstar --ntasks=1 --array=0-32 --time=08:00:00 FillSpinAlignment.slr # production mode
+  # sbatch -p shared-chos --account rhstar --ntasks=1 --array=0-9 --time=01:00:00 FillSpinAlignment.slr # test mode
+  sbatch -p shared-chos --account rhstar --ntasks=1 --array=0-32 --time=08:00:00 FillSpinAlignment.slr # production mode
+
+  # sbatch -p shared-chos --account rhstar --ntasks=1 --array=0-9 --time=02:00:00 FillSpinAlignmentZdcSmd.slr # test mode for ZDCSMD
+  # sbatch -p shared-chos --account rhstar --ntasks=1 --array=0-32 --time=08:00:00 FillSpinAlignmentZdcSmd.slr # production mode for ZDCSMD
 else
   echo "Wrong number of parameters!!"
 fi
