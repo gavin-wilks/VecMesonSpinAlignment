118389adc618fc80c2645e097f0a1935ca19054f
gavin.wilks <gavin.wilks@gmail.com>
2025-01-02 16:06:07 -0500

    ToF tagging method for tof matching efficiency

---
+++ TreeProductionToFMatching/submit/19GeV_2019/submitAll.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./submitTreeProduction.sh 0 SE
+
+#./submitTreeProduction.sh 1 ME
---
+++ TreeProductionToFMatching/submit/19GeV_2019/submitTreeProduction.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+codePath=/star/u/gwilks3/Workspace/VectorMesonSpinAlignment/VecMesonSpinAlignment/TreeProductionToFMatching
+
+##########Energy Selection##########
+# energy=0  # 200GeV
+# library=SL18h
+# listPath=/star/u/sunxuhit/WorkSpace/VecMesonSpinAlignment_BESII/FileList/200GeV_2014
+# outPath=/star/data01/pwg/sunxuhit/AuAu200GeV_2014
+ 
+# energy=1  # 54.0GeV
+# library=SL18c
+# listPath=/star/u/sunxuhit/WorkSpace/VecMesonSpinAlignment_BESII/FileList/54GeV_2017
+# outPath=/star/data01/pwg/sunxuhit/AuAu54GeV_2017
+
+energy=4  # 19GeV
+library=SL23d
+listPath=/star/u/gwilks3/Workspace/VectorMesonSpinAlignment/PidFlow/FileList/19p6GeV_2019
+outPath=/gpfs01/star/pwg/gwilks3/VectorMesonSpinAlignment/AuAu19GeV_2019
+##########Energy Selection##########
+
+##########Mode Selection##########
+#mode=0
+#outDir=ReCenterParameter_TPCOnly
+
+#mode=1
+#outDir=ShiftParameter
+
+#mode=2
+#outDir=Resolution
+
+mode=3
+#mode=4
+#outDir=Phi/Forest
+##########Mode Selection##########
+
+##########Mixed Event Selection##########
+flag_ME=$1 # 0 for SE | 1 for ME
+SM=$2
+#flag_ME=1 # 0
+#SM=ME
+##########Mixed Event Selection##########
+
+flag_PID=0 # 0 for phi | 1 for rho | 2 for kstar
+
+
+#outDir=Phi/Forest/Phi${SM}_eta1_20240724_pt0p9m2only
+outDir=Phi/Forest/Phi${SM}_eta1_20240925_TPCTOF_Tagged_NOTcent2060_extrainfo
+#outDir=Phi/Forest/Phi${SM}_eta1_20240918_TPCTOF_cent2060_extrainfo
+#outDir=Phi/Forest/Phi${SM}_eta1_20240808_PIDEff_DCA2_MoreEtaBins_NoPhiBins_Fixed
+#outDir=Phi/Forest/Phi_KaonPID_20240724
+
+mkdir -p JOBS/report
+mkdir -p JOBS/csh
+mkdir -p JOBS/list
+
+mkdir -p ${outPath}/Log/SpinAlignment/${outDir}
+mkdir -p ${outPath}/OutPut/SpinAlignment/${outDir}
+
+##########Test Production##########
+star-submit-template -template ${codePath}/submit/19GeV_2019/testProductionTemp.xml -entities mode=$mode,energy=$energy,flag_ME=$flag_ME,flag_PID=$flag_PID,SM=$SM,library=$library,codePath=$codePath,outPath=$outPath,listPath=$listPath,outDir=$outDir
+##########Test Production##########
+
+##########Full Production##########
+# star-submit-template -template TreeProductionTemp.xml -entities mode=$mode,energy=$energy,flag_ME=$flag_ME,SM=$SM,library=$library,codePath=$codePath,outPath=$outPath,listPath=$listPath,outDir=$outDir
+##########Full Production##########
+
+##########Re-Submit##########
+#star-submit-template -template resubmitProductionTemp.xml -entities mode=$mode,energy=$energy,flag_ME=$flag_ME,flag_PID=$flag_PID,SM=$SM,library=$library,codePath=$codePath,outPath=$outPath,listPath=$listPath,outDir=$outDir
+
+##########Re-Submit##########
